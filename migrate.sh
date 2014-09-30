6873d4e113c79694d10a8f46a5aa4875ccd05593
Benjamin Boumendil <benjamin@emencia.com>
2014-09-30 17:59:55 +0200

    correct var error

--- migrate.sh
+++ migrate.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 if [ $1 ]; then
-    if [ $2 = 'init' ]; then
-    	bin/django-instance schemamigration $1 --initial
-    	bin/django-instance migrate $1
-    elif [ $2 = 'fake' ]; then
+    if [ '$2' = 'init' ]; then
+        bin/django-instance schemamigration $1 --initial
+        bin/django-instance migrate $1
+    elif [ '$2' = 'fake' ]; then
         bin/django-instance migrate $1 --fake
     else
-    	bin/django-instance schemamigration $1 --auto
-    	bin/django-instance migrate $1
+        bin/django-instance schemamigration $1 --auto
+        bin/django-instance migrate $1
     fi
 else
     echo "Usage : migr [AppName] [option]
