03cb5e316692b0b869cf10714aea37427e63a4cb
benjaminboumendil <benj_6@hotmail.fr>
2014-10-07 15:42:36 +0200

    update migrate.sh, ghost and list option

--- migrate.sh
+++ migrate.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 if [ $1 ]; then
-    if [ '$2' = 'init' ]; then
+    if [ $2 = 'init' ]; then
         bin/django-instance schemamigration $1 --initial
         bin/django-instance migrate $1
-    elif [ '$2' = 'fake' ]; then
+    elif [ $2 = 'fake' ]; then
         bin/django-instance migrate $1 --fake
-    elif [ '$2' = 'update' ]; then
+    elif [ $2 = 'update' ]; then
         bin/django-instance schemamigration $1 --auto --update
+    elif [ $2 = 'ghost' ]; then
+        django-instance migrate $1 --delete-ghost-migrations
+    elif[ $2 = 'list' ]; then
+        django-instance migrate $1 --list
     else
         bin/django-instance schemamigration $1 --auto
         bin/django-instance migrate $1
@@ -17,5 +21,8 @@ else
 
 options : 
     init : for initial migration
-    fake : for fake migration"
+    fake : fake migration
+    update : update last migration
+    ghost : delete ghost migration
+    list : list all migration"
 fi
